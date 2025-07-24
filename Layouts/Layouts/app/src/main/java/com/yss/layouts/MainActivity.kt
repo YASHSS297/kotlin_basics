804a7bfd2d571455e134b243d7b143f793c466de
Yash Shinde <shindeyash297@gmail.com>
2025-07-24 12:15:06 +0530

    Add files via upload

---
+++ Layouts/Layouts/app/src/main/java/com/yss/layouts/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.yss.layouts
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var exam : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        exam = findViewById(R.id.Example)
+
+        exam.setTextColor(Color.BLACK)
+        exam.setText("Use Me...")
+exam.setOnClickListener {
+
+    exam.setText("hello yash")
+}
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+
+
+
+
